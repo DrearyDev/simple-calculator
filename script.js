19bb4e42177f791cdde72df84d86841a77d82c8f
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-10-11 14:32:44 -0400

    Add basic functionality to the calculator

--- script.js
+++ script.js
@@ -3,10 +3,18 @@
 const numbers = document.querySelectorAll('.number');
 const display = document.querySelector('.display');
 const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals');
 
-
+display.textContent = 0;
 let displayValue = null;
-display.textContent = displayValue;
+
+let firstNum = null;
+let secondNum = 0;
+let answer = null;
+
+let lastOperator = null;
+let currentOperator = null;
 
 
 
@@ -17,18 +25,77 @@ numbers.forEach(number => {
 });
 
 function handleDisplay(number){
-    display.textContent += number;
-    displayValue = display.textContent;
+
+    if (displayValue === null){
+        display.textContent = number;
+        displayValue = display.textContent;
+    } else {
+        display.textContent += number;
+        displayValue = display.textContent;
+    };
+
 };
 
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
-        console.log(displayValue);
+
+        currentOperator = e.target.textContent;
+
+        if (firstNum === null) {
+            firstNum = displayValue;
+            displayValue = null;
+            lastOperator = currentOperator;
+        } else {
+            secondNum = displayValue;
+            firstNum = operate(firstNum, lastOperator, secondNum);
+            secondNum = null;
+
+            displayValue = null;
+            handleDisplay(firstNum);
+            displayValue = null;
+            
+            lastOperator = currentOperator;
+        };
+
+        console.log(firstNum, currentOperator, secondNum, '=', answer );
+
     });
+});
+
+clear.addEventListener('click', (e) => {
+    display.textContent = 0;
+    displayValue = null;
+    firstNum = null;
+    secondNum =  null;
+    answer = null;
+    currentOperator = null;
+});
+
+equals.addEventListener('click', () => {
+    secondNum = displayValue;
+    answer = operate(firstNum, currentOperator, secondNum);
+    firstNum = null;
+    displayValue = null;
+    handleDisplay(answer);
 });
 
 
 
+
+function operate(num1, operator, num2){
+    switch (operator) {
+        case '+':
+            return add(+num1, +num2);
+        case '-':
+            return subtract(+num1, +num2);
+        case '*':
+            return multiply(+num1, +num2);
+        case '/':
+            return divide(+num1, +num2);
+        default:
+            return 'error';
+    };
+};
 
 function add(num1, num2){
     return num1 + num2;
@@ -44,25 +111,6 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
     if (num2 === 0) return 'lmao';
-    
-    return num1 / num2
-};
 
-function operate(num1, operator, num2){
-    switch (operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
-            break;
-        default:
-            return error;
-    };
+    return num1 / num2
 };
